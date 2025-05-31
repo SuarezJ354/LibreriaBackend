2855cb0649c0ab19481aa16b34c600ace009a6d4
Jesus Suarez <jsuarezho@cesde.net>
2025-05-31 11:00:57 -0500

    Transactional 2

--- src/main/java/com/cesde/library/Controlador/CapituloController.java
+++ src/main/java/com/cesde/library/Controlador/CapituloController.java
@@ -4,6 +4,7 @@ import com.cesde.library.Modelo.Capitulo;
 import com.cesde.library.Servicios.CapituloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +20,29 @@ public class CapituloController {
 
     // 📌 Listar capítulos de un libro
     @GetMapping("/{libroId}")
+    @Transactional(readOnly = true)
     public ResponseEntity<List<Capitulo>> obtenerCapitulosPorLibro(@PathVariable Long libroId) {
         return ResponseEntity.ok(capituloService.obtenerCapitulosPorLibro(libroId));
     }
 
     // 📌 Listar capítulos gratuitos de un libro
     @GetMapping("/{libroId}/gratuitos")
+    @Transactional(readOnly = true)
     public ResponseEntity<List<Capitulo>> obtenerCapitulosGratuitos(@PathVariable Long libroId) {
         return ResponseEntity.ok(capituloService.obtenerCapitulosGratuitos(libroId));
     }
 
     // 📌 Obtener un capítulo específico por número
     @GetMapping("/{libroId}/{numeroCapitulo}")
+    @Transactional(readOnly = true)
     public ResponseEntity<Capitulo> obtenerCapituloPorNumero(@PathVariable Long libroId, @PathVariable Integer numeroCapitulo) {
         Optional<Capitulo> capitulo = capituloService.obtenerCapituloPorNumero(libroId, numeroCapitulo);
         return capitulo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-
-
     // 📌 Registrar un nuevo capítulo
     @PostMapping
+    @Transactional
     public ResponseEntity<Capitulo> registrarCapitulo(@RequestBody Capitulo capitulo) {
         Capitulo nuevoCapitulo = capituloService.guardarCapitulo(capitulo);
         return ResponseEntity.ok(nuevoCapitulo);
@@ -47,12 +50,14 @@ public class CapituloController {
 
     // 📌 Eliminar capítulo por ID
     @DeleteMapping("/{id}")
+    @Transactional
     public ResponseEntity<String> eliminarCapitulo(@PathVariable Long id) {
         capituloService.eliminarCapitulo(id);
         return ResponseEntity.ok("Capítulo eliminado correctamente");
     }
 
     @PutMapping("/{id}")
+    @Transactional
     public ResponseEntity<Capitulo> actualizarCapitulo(@PathVariable Long id, @RequestBody Capitulo capitulo) {
         capitulo.setId(id); // Asegurar que el ID coincida
         Capitulo capituloActualizado = capituloService.guardarCapitulo(capitulo);
